7f5df0a72666a4b69e9f4954fe1002a19df21b9b
dcmunhoz <dc.munhoz@hotmail.com>
2020-01-15 14:01:32 -0300

    Day3 - Initial React.JS

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Componente:   Bloco isolado de HTML, CSS e JS que não interfere no restante da aplicação.
+// Propriedade:  Informações que um componente pai passa pra o componente filho.
+// Estado:       Informações mantidas pelo componente (Lembrar: Imutabilidade).
+
+function App() {
+  return (
+    <h1>Olá React</h1>
+  );
+}
+
+export default App;
